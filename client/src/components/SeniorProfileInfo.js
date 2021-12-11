440816f742c31493c22b1f8c11abdd7e2992697c
880036 <73724407+880036@users.noreply.github.com>
2021-12-11 19:37:21 +0100

    Add senior availability data grid

--- client/src/components/SeniorProfileInfo.js
+++ client/src/components/SeniorProfileInfo.js
@@ -8,6 +8,28 @@ import Images from "../Constants/Images";
 import Texts from "../Constants/Texts";
 import ConfirmDialog from "./ConfirmDialog";
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+let toDayName = (index, language) => {
+  const days = Texts[language].availabilityWeekModal;
+  switch (index) {
+    case 0: return days.monday; break;
+    case 1: return days.tuesday; break;
+    case 2: return days.wednesday; break;
+    case 3: return days.thursday; break;
+    case 4: return days.friday; break;
+    case 5: return days.saturday; break;
+    case 6: return days.sunday; break;
+    default: return "";
+  }
+}
+
 class SeniorProfileInfo extends React.Component {
   state = { weekModalIsOpen: false, confirmDialogIsOpen: false, deleteIndex: "" };
 
@@ -31,12 +53,13 @@ class SeniorProfileInfo extends React.Component {
     const {
       senior,
       language,
-      availabilities,
       otherInfo,
       gender,
       birthdate,
       showAdditional
     } = this.props;
+
+    const availabilities = senior.availabilities;
 
     const { confirmDialogIsOpen, weekModalIsOpen } = this.state;
     const texts = Texts[language].seniorProfileInfo;
@@ -101,6 +124,35 @@ class SeniorProfileInfo extends React.Component {
             )}
           </div>
         )}
+
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 300 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Giorno</TableCell>
+              <TableCell align="center">Orario</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {availabilities.map((row) => (
+              <TableRow
+                key={row.weekDay}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {toDayName(row.weekDay, language)}
+                </TableCell>
+                <TableCell align="center">
+                    dalle {row.startTimeHour} : {row.startTimeMinute} 
+                    <br/>
+                    alle {row.endTimeHour} : {row.endTimeMinute} 
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
       </React.Fragment>
     );
   }
